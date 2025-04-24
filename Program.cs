cae65b265c89b84b49e3c2fc8be09819b89dc1ce
thaiguer <thaiguer.k@gmail.com>
2025-04-23 22:37:45 -0300

    add changes on B1

--- Program.cs
+++ Program.cs
@@ -2,3 +2,10 @@
 Console.WriteLine("Good day.");
 Console.WriteLine("Change on Main Branch.");
 Console.WriteLine("It enough of Good Day.");
+
+Console.WriteLine("B1 change 1");
+Console.WriteLine("B1 change 2");
+Console.WriteLine("B1 change 3");
+Console.WriteLine("B1 change 4");
+Console.WriteLine("B1 change 5");
+Console.WriteLine("B1 change 6");
